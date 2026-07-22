55cf1281d45c60a42b61d89f16845f29277927db
SergiuStaf90 <sergiu.stafie@endava.com>
2026-07-22 16:50:54 +0300

    Add username locator

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,9 +1,10 @@
-import { Page } from '@playwright/test';
+import { Locator, Page } from '@playwright/test';
 
 export class LoginPage {
 
+    private readonly usernameInput: Locator;
+
     constructor(private readonly page: Page) {
-
+        this.usernameInput = page.locator('#user-name');
     }
-
 }
